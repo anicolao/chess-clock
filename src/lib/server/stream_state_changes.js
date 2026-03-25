3e3aa4c0edb7a4c534d0b01fc5f3a43de5ff2c95
Alex Nicolaou <anicolao@gmail.com>
2026-03-25 11:34:59 -0400

    Halve quiet frame diff cutoff again

--- src/lib/server/stream_state_changes.js
+++ src/lib/server/stream_state_changes.js
@@ -55,7 +55,7 @@ function computeMad(values, center) {
 	return median(values.map((value) => Math.abs(value - center)));
 }
 
-const DIFF_PIXEL_THRESHOLD = 6;
+const DIFF_PIXEL_THRESHOLD = 3;
 
 function computeFrameDiffMetrics(previousGray, currentGray) {
 	const diff = new cv.Mat();
